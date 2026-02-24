bf631b7c893cc7f90d528ed1515030a6a8534196
SrStamm <mirkostamm19@gmail.com>
2026-02-24 22:40:57 +0000

    class Utilizador and buscarUtilizadores() created

---
+++ lista-utilizadores/script.js
@@ -0,0 +1,24 @@
+class Utilizador {
+  constructor(nome, email) {
+    this.nome = nome;
+    this.email = email;
+  }
+
+  async buscarUtilizadores() {
+    const listaUtilizadores = [];
+
+    try {
+      const response = await fetch();
+      const json = await response.json();
+
+      json.array.forEach((element) => {
+        const newUtilizador = new Utilizador(element);
+        listaUtilizadores.push(newUtilizador);
+      });
+
+      return listaUtilizadores;
+    } catch (error) {
+      return [];
+    }
+  }
+}
