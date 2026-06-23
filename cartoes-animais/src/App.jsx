55ac1349377de8ad35406bc06181c7cd0d0ff023
SrStamm <mirkostamm19@gmail.com>
2026-06-23 18:44:36 +0100

    atualizado style do CartaoAnimal e App components

--- cartoes-animais/src/App.jsx
+++ cartoes-animais/src/App.jsx
@@ -2,10 +2,12 @@ import CartaoAnimal from "./CartaoAnimal";
 
 function App() {
   return (
-    <div className="">
-      <h1>Os Nossos Animais de Estimação</h1>
+    <div className="bg-slate-100 min-h-screen p-8">
+      <h1 className="text-3xl font-bold text-center mb-8">
+        Os Nossos Animais de Estimação
+      </h1>
 
-      <div className="flex gap-1">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <CartaoAnimal nome="Fernet" especie="Gato" idade="1 ano" dono="Maria" />
         <CartaoAnimal
           nome="Prince"
@@ -13,14 +15,14 @@ function App() {
           idade="12 anos"
           dono="Maria"
         />
-        <CartaoAnimal nome="Emily" especie="Cão" idade="4 anos" dono="Ana" />
+        <CartaoAnimal nome="Emily" especie="Coelho" idade="4 anos" dono="Ana" />
         <CartaoAnimal
           nome="Estrela"
-          especie="Cão"
+          especie="Gato"
           idade="4 anos"
           dono="Daniel"
         />
-        <CartaoAnimal nome="Nina" especie="Cão" idade="4 anos" dono="João" />
+        <CartaoAnimal nome="Nina" especie="Pássaro" idade="1 ano" dono="João" />
       </div>
     </div>
   );
--- cartoes-animais/src/CartaoAnimal.jsx
+++ cartoes-animais/src/CartaoAnimal.jsx
@@ -1,11 +1,21 @@
 function CartaoAnimal(props) {
   return (
-    <div className="bg-white rounded-xl shadow-md">
-      <div className="rounded-full w-12 h-12">{props.nome.charAt(0)}</div>
-      <h2>Nome: {props.nome}</h2>
-      <p>Especie: {props.especie}</p>
-      <p>Idade: {props.idade}</p>
-      <p>Dono: {props.dono}</p>
+    <div className="bg-white rounded-xl shadow-md p-5 flex flex-col gap-3">
+      <div className="rounded-full w-12 h-12 bg-blue-500 text-white text-xl font-bold flex items-center justify-center ">
+        {props.nome.charAt(0)}
+      </div>
+      <h2 className="text-xl">{props.nome}</h2>
+      <p className="text-gray-600 text-sm">
+        <span className="font-semibold text-gray-800">Espécie: </span>
+        {props.especie}
+      </p>
+      <p className="text-gray-600 text-sm">
+        <span className="font-semibold text-gray-800">Idade: </span>
+        {props.idade}
+      </p>
+      <p className="text-gray-600 text-sm">
+        <span className="font-semibold text-gray-800">Dono: </span> {props.dono}
+      </p>
     </div>
   );
 }
