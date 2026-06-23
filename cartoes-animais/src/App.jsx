d4c646813046070af6a433877156093f0f0e26ab
SrStamm <mirkostamm19@gmail.com>
2026-06-23 18:29:44 +0100

    agregado prop dono e agregado mais dois animais

--- cartoes-animais/src/App.jsx
+++ cartoes-animais/src/App.jsx
@@ -5,8 +5,22 @@ function App() {
     <div className="">
       <h1>Os Nossos Animais de Estimação</h1>
 
-      <div className="flex">
-        <CartaoAnimal nome="Fernet" especie="Cão" idade="4" />
+      <div className="flex gap-1">
+        <CartaoAnimal nome="Fernet" especie="Gato" idade="1 ano" dono="Maria" />
+        <CartaoAnimal
+          nome="Prince"
+          especie="Cão"
+          idade="12 anos"
+          dono="Maria"
+        />
+        <CartaoAnimal nome="Emily" especie="Cão" idade="4 anos" dono="Ana" />
+        <CartaoAnimal
+          nome="Estrela"
+          especie="Cão"
+          idade="4 anos"
+          dono="Daniel"
+        />
+        <CartaoAnimal nome="Nina" especie="Cão" idade="4 anos" dono="João" />
       </div>
     </div>
   );
--- cartoes-animais/src/CartaoAnimal.jsx
+++ cartoes-animais/src/CartaoAnimal.jsx
@@ -1,10 +1,11 @@
 function CartaoAnimal(props) {
   return (
     <div className="bg-white rounded-xl shadow-md">
-      <div className="rounded">{props.nome.charAt(0)}</div>
-      <h2>{props.nome}</h2>
-      <p>{props.especie}</p>
-      <p>{props.idade}</p>
+      <div className="rounded-full w-12 h-12">{props.nome.charAt(0)}</div>
+      <h2>Nome: {props.nome}</h2>
+      <p>Especie: {props.especie}</p>
+      <p>Idade: {props.idade}</p>
+      <p>Dono: {props.dono}</p>
     </div>
   );
 }
